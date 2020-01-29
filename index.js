a0a79cb11a1750b1a2f60fcfc897bb5b684a488b
Alexander Talberg <Alexandermtalberg@gmail.com>
2020-01-28 22:33:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Greblat2',
+    password: '',
     port: 3306,
     database: 'employee_db'
 
